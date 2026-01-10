f73f16547fde48a712c33b4431785f080b8efd75
onebostoncapital <69724712+onebostoncapital@users.noreply.github.com>
2026-01-10 20:56:06 +0100

    Create anomaly_detection.py

---
+++ ai_modules/anomaly_detection.py
@@ -0,0 +1,6 @@
+# Master Rule Book: Pool Anomaly Detection (Whale Tracking)
+class AnomalyDetector:
+    def check_for_whales(self, onchain_data):
+        # This will eventually detect if a 'Whale' is moving big money
+        # For now, it returns 'Safe'
+        return {"status": "Safe", "anomaly_score": 0.0}
